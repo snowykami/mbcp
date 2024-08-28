e940ff7a64197bba304cab885b51368206b5e0a1
snowykami <snowykami@outlook.com>
2024-08-28 12:35:22 +0000

    Deploying to docs from @ snowykami/mbcp@0320f13ae94700c8dd360db43593d8106874601f 🚀

---
+++ assets/ja_api_index.md.4BnflFIm.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./chunks/framework.BV61Qrc0.js";const _=JSON.parse('{"title":"mbcp","description":"","frontmatter":{"title":"mbcp"},"headers":[],"relativePath":"ja/api/index.md","filePath":"ja/api/index.md"}'),n={name:"ja/api/index.md"};function i(r,c,o,p,s,d){return a(),t("div")}const f=e(n,[["render",i]]);export{_ as __pageData,f as default};
