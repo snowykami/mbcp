d3a21b89fbe1b0b46e1ede6ef9addfcb9aac45ec
snowykami <snowykami@outlook.com>
2024-08-29 05:25:00 +0000

    Deploying to docs from @ snowykami/mbcp@260a0f870921b642d9427d69abcc7c97d87ebd59 🚀

---
+++ assets/zht_api_index.md.Dq4XPUKZ.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./chunks/framework.BV61Qrc0.js";const _=JSON.parse('{"title":"mbcp","description":"","frontmatter":{"title":"mbcp"},"headers":[],"relativePath":"zht/api/index.md","filePath":"zht/api/index.md"}'),n={name:"zht/api/index.md"};function i(r,c,o,p,s,d){return a(),t("div")}const f=e(n,[["render",i]]);export{_ as __pageData,f as default};
