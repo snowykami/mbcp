5a81a8c3ff9d17a42a0eafea003dee830919d1cb
Snowykami <snowykami@outlook.com>
2024-08-30 20:39:38 +0800

    :memo: Update mkdoc.bat

--- mkdoc.bat
+++ mkdoc.bat
@@ -1,4 +1,5 @@
 chcp 65001
+
 python -m litedoc mbcp -o docs/zh/api -l zh-Hans -f editLink=false  -cd class -fd def -md def -vd var -cs
 python -m litedoc mbcp -o docs/en/api -l en -f editLink=false       -cd class -fd def -md def -vd var -cs
 python -m litedoc mbcp -o docs/ja/api -l ja -f editLink=false       -cd class -fd def -md def -vd var -cs
