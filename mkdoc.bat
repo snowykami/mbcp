0c2d342bbfc9c0d0e1b96b7fe651e9713c671a4d
snowykami <snowykami@outlook.com>
2024-08-31 06:07:58 +0800

    :fire: 移除文档构建脚本

--- mkdoc.bat
+++
@@ -1,5 +0,0 @@
-chcp 65001
-litedoc mbcp -o docs/zh/api -l zh-Hans    -cd class -fd def -md def -vd var -cs
-litedoc mbcp -o docs/en/api -l en         -cd class -fd def -md def -vd var -cs
-litedoc mbcp -o docs/ja/api -l ja         -cd class -fd def -md def -vd var -cs
-litedoc mbcp -o docs/zht/api -l zh-Hant   -cd class -fd def -md def -vd var -cs
